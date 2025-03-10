4b333de84d3a77375160ff902663d18a3778f6a2
cbrookscode <colbybrooks@myyahoo.com>
2025-03-10 13:58:03 -0400

    added gitignore and repl functionality

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import(
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -13,5 +15,20 @@ func cleanInput(text string) []string {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("Pokedex > ")
+		
+		// check if obtaining user input was successful. Clean input if so, else check for error. If user input wasn't nothing, print first word.
+		var user_words []string
+		if scanner.Scan() {
+			user_words = cleanInput(scanner.Text())
+		} else if err := scanner.Err(); err != nil {
+			fmt.Errorf("There was an error: %w", err)
+		}
+		
+		if len(user_words) != 0 {
+			fmt.Printf("Your command was: %v\n", user_words[0])
+		}
+	}
 }
